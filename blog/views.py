8e600efdb701db44879a03084cb7d27e4c49e828
lenovo <smithmayowa20@gmail.com>
2017-05-11 02:32:04 +0100

    new one

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import Post
 from django.utils import timezone
 
-posts = Post.objects.filter(published_date__lte=timezone.now())
+posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 #context = {"posts":"Hi There Motherfucker",'green':"Hello there how are you"}
 # Create your views here.
 def post_list(request):
