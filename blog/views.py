acd4ac51351786d3ddb1383420a2f5688bf60a95
lenovo <smithmayowa20@gmail.com>
2017-05-11 02:00:52 +0100

    another commit

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
 
+posts = Post.objects.filter(published_date__lte=timezone.now())
+#context = {"posts":"Hi There Motherfucker",'green':"Hello there how are you"}
 # Create your views here.
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+	 return(render(request,'blog/post_list.html',{'posts':posts}))
+	
+def greeting(request):
+     return (render(request,'blog/greeting.html',{}))	
+	
